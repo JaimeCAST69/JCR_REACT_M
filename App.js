162153ee7fd90dc63f095ec202203fdea8173621
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-07-08 20:27:03 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/modules/auth/screens/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets put that pain to use!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <Login />
+    
+    /* Uncomment the following lines to see the layout with colored boxes
+    <View style={{flex:1, backgroundColor: 'red', flexDirection: 'row'}}>
+      <View style={{flex: 1, backgroundColor: 'blue'}}/>
+      <View style={{flex: 1, backgroundColor: 'white'}}/>
+      <View style={{flex: 1, backgroundColor: 'red'}}/>
+    </View>*/
+   /* <View style={{flex:1, backgroundColor: 'red', flexDirection: 'row'}}>
+      <View style={{flex: 1, backgroundColor: 'blue'}}/>
+      <View style={{flex: 1, backgroundColor: 'white'}}/>
+      <View style={{flex: 1, backgroundColor: 'red'}}/>
+    </View>*/
   );
 }
 
---
+++ src/modules/auth/screens/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Input, Image, Button } from '@rneui/base';
+
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+        <Image
+            source={{ uri: 'https://i.blogs.es/fc31e6/github-copilot-1/450_1000.jpeg' }}
+            style={{ width: 200, height: 200, marginBottom: 20 }}
+        />
+        <Text>Login Screen</Text>
+        <Input 
+        keyboardType='email-address'
+        placeholder="Correo electrónico"
+        containerStyle={{width: '80%', height: 42, marginVertical: 16 }}/>
+        <Input
+        containerStyle={{width: '80%', height: 42, marginVertical: 16 }}
+        placeholder="Contraseña" secureTextEntry />
+        <Button title="Iniciar Sesion" containerStyle={{width:'80%',height: 42, marginTop: 64}} onPress={() => alert('Lets put that pain to use')} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
